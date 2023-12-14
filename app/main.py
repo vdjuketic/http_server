7eaf72d864ee08b9b7dc013b1a72eab83eafaead
vdjuketic <v.djuketic@gmail.com>
2023-12-14 12:00:06 +0100

    third stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    conn, address = server_socket.accept()  # wait for client
+
+    conn.recv(1024)
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.close()
 
 
 if __name__ == "__main__":
