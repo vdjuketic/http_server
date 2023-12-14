ceec9efe0aa3c8d8fc69b05ecd8600d6e25ea215
vdjuketic <v.djuketic@gmail.com>
2023-12-14 12:01:48 +0100

    third stage

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, address = server_socket.accept()  # wait for client
 
-    conn.recv(1024)
+    message = conn.recv(1024)
+
+    print(message)
+
     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     conn.close()
 
